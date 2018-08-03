84b53d8ca4db08786c45cd456569880b3eebc822
haeju-dev <haeju.dev@gmail.com>
2018-08-04 02:39:00 +0900

    output update

--- mytime.py
+++ mytime.py
@@ -11,7 +11,11 @@ def toutc(timestamp = None):
     return {'strf' : utc, 'timestamp' : t.timestamp(), 'datetime' : t}
 
 def togmt9(timestamp = None):
-    utc = toutc(timestamp)['datetime']
+    utcdata = toutc(timestamp)
+    utc = utcdata['datetime']
     gmt9 = utc + datetime.timedelta(hours=9)
     gmt9_strf = gmt9.strftime("%Y-%m-%d %H:%M:%S m : %f, +09:00 (UTC)")
-    return {'strf' : gmt9_strf, 'timestamp' : gmt9.timestamp(), 'datetime' : gmt9}
+    data = dict()
+    data.update({'utc' : utcdata})
+    data.update({'gmt9' : {'strf' : gmt9_strf, 'timestamp' : gmt9.timestamp(), 'datetime' : gmt9}})
+    return data
